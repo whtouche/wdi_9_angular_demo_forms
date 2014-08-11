24a84c027d4e41cb6c0521c5a523fe7e34898437
Tom Dyer <tdyer1@gmail.com>
2014-08-11 09:48:48 -0400

    Form to create a customer

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -6,6 +6,8 @@
     $scope.customers= [];
     $scope.appSettings = appSettings;
 
+    $scope.master = {};
+
     function init(){
       // Init the customers from the factory
       //$scope.customers = customersFactory.getCustomers();
@@ -20,6 +22,20 @@
     }
 
     init();
+
+    $scope.update = function(customer){
+      $scope.master = angular.copy(customer);
+
+    };
+    $scope.reset = function(){
+      $scope.customer = angular.copy($scope.master);
+    };
+
+    $scope.isUnchanged = function(customer) {
+      return angular.equals(customer, $scope.master);
+    };
+
+    $scope.reset();
 
     $scope.doSort = function(propName){
       $scope.sortBy = propName;
